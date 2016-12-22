94934a46b7a92ca6d3dbdc62b3206f91d77df81b
alexsando86 <1003842@SKP1003842MN001.local>
2016-12-22 09:18:13 +0900

    js탭추가

--- ui.js
+++ ui.js
@@ -1,10 +1,27 @@
 UI = {};
 
 (function(){
-	var a = UI.tab = function(){
-		console.log('1')
+	var a = UI.tab = function(tabID){
+		var tabWrap = $(tabID), tabList = tabWrap.find('ul:first-of-type li');
+
+		function hideContentDiv(){
+			for(var i=0; i<tabList.length; i++){
+				tabWrap.find($(tabList[i]).find('a').attr('href')).hide().removeClass('active');
+			}
+		}
+		hideContentDiv();
+
+		tabList.find('a').on('click', function(){
+			hideContentDiv();
+			tabWrap.find($(this).attr('href')).show().addClass('active');			
+		});
+		tabWrap.find('#tab_navi li:first-child a').click();
 	}
 	var test = function(){
 		console.log('2')
 	}
 })();
+
+
+
+
