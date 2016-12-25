b75e5eb22b560711013b9fd1865811b7b37ee231
DESKTOP-62BO1GJ\AlexYuk <yukseondo@naver.com>
2016-12-25 22:41:44 +0900

    VisualStudio Test

--- ui.js
+++ ui.js
@@ -4,15 +4,15 @@ UI = {};
 	var a = UI.tab = function(tabID){
 		var tabWrap = $(tabID), tabList = tabWrap.find('ul:first-of-type li');
 
-		function hideContentDiv(){
+		function hidecontentsDiv(){
 			for(var i=0; i<tabList.length; i++){
 				tabWrap.find($(tabList[i]).find('a').attr('href')).hide().removeClass('active');
 			}
 		}
-		hideContentDiv();
+		hidecontentsDiv();
 
 		tabList.find('a').on('click', function(){
-			hideContentDiv();
+			hidecontentsDiv();
 			tabWrap.find($(this).attr('href')).show().addClass('active');			
 		});
 		tabWrap.find('#tab_navi li:first-child a').click();
