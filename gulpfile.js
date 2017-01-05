ee74477d50876d5cd2bd74e1e1d9f24981848918
alexsando86 <1003842@SKP1003842MN001.local>
2017-01-05 15:19:23 +0900

    GulpFile.JS 이미지 sprite추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,34 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var livereload = require('gulp-livereload');
+var buffer = require('vinyl-buffer');
+var csso = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var merge = require('merge-stream');
 
-var paths = {
-	'MW': 'm/MW/'
-}
+var spritesmith = require('gulp.spritesmith');
 
-gulp.task('check', function(){
-	return gulp.src([paths.MW + 'css/**/*.css', paths.MW + 'html/**/*.html'])
-	.pipe(livereload());
+
+// https://www.npmjs.com/package/gulp.spritesmith
+gulp.task('sprite', function () {
+    // Generate our spritesheet
+    var spriteData = gulp.src('img/sprite/*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css'
+    }));
+
+    // Pipe image stream through image optimizer and onto disk
+    var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+        .pipe(buffer())
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'));
+
+    // Pipe CSS stream through CSS optimizer and onto disk
+    var cssStream = spriteData.css
+        // .pipe(csso()) CSS를 압축한다.
+        .pipe(gulp.dest('css/'));
+
+    // Return a merged stream to handle both `end` events
+    return merge(imgStream, cssStream);
 });
 
-gulp.task('watch', function(){
-	livereload.listen();
-	//gulp.watch(paths.MW + '*', ['check']);
-	gulp.watch(paths.MW + 'css/**/*.css', ['check']);
-	gulp.watch(paths.MW + 'html/**/*.html', ['check']);
-});
-
-gulp.task('default', ['check', 'watch'])
-
-
-
-// var gulp        = require('gulp');
-// var browserSync = require('browser-sync').create();
-
-// // process JS files and return the stream.
-// gulp.task('html', function () {
-//     return gulp.src('html/**/*.html')
-//     .pipe(browserSync.reload());
-// });
-
-// // use default task to launch Browsersync and watch html files
-// gulp.task('server', function () {
-
-//     // Serve files from the root of this project
-//     gulp.task('browser-sync', function() {
-// 	    browserSync.init({
-// 	        proxy: "m/MW"
-// 	    });
-// 	});
-//     // add browserSync.reload to the tasks array to make
-//     // all browsers reload after tasks are complete.
-//     gulp.watch("html/**/*.html", ['html']);
-// });
-
-// gulp.task('default', ['server'])
+gulp.task('default', ['sprite']);
