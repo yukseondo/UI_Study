328118dc18fde8cff56736a6acc24041e57f5d98
육선도 <1003842@SKP1003842MN001.local>
2017-02-28 10:58:39 +0900

    add file study

---
+++ inside.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 1003842 on 2017. 2. 24..
+ */
+
+
+var i = 0;
+for(i=0; i<=10; i++){
+    document.write('Hello javascript');
+    document.write('<br>');
+}
+
+
+<div style="width:375px;height:662px;margin:0 auto;padding:75px 0 0 17px;box-sizing:border-box;background:url('http://wiki.skplanet.com/download/attachments/111474671/iphone6-portrait3.png?version=1&modificationDate=1487927177000&api=v2') no-repeat 0 0">
+    <iframe src="http://s-m.011st.com/MW/html/lego/component/accordion.html" width="340" height="505" border="0" frameborder="0"></iframe>
+    </div>
