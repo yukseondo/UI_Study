79f8a704616da14e25bca47aa4061645ea618e44
육선도 <alexsando86@sk.com>
2017-03-20 17:03:19 +0900

    gulpfileJS modify

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,25 @@ var buffer = require('vinyl-buffer');
 var csso = require('gulp-csso');
 var imagemin = require('gulp-imagemin');
 var merge = require('merge-stream');
-var sass = require('gulp-sass');
-
 var spritesmith = require('gulp.spritesmith');
+var server = require('gulp-server-livereload');
+// var sass = require('gulp-sass');
+
 
 
 // Define our tasks
-gulp.task('sass', function generateSass () {
-    gulp.src('css/*.scss')
-        .pipe(sass({outputStyle: 'expanded'}))  //outputStyle : expanded, compact, compressed
-        .pipe(gulp.dest('scss/'));
-});
+// gulp.task('sass', function generateSass () {
+//     gulp.src('css/*.scss')
+//         .pipe(sass({outputStyle: 'expanded'}))  //outputStyle : expanded, compact, compressed
+//         .pipe(gulp.dest('scss/'));
+// });
 
 // https://www.npmjs.com/package/gulp.spritesmith
 gulp.task('sprite', function () {
     // Generate our spritesheet
     var spriteData = gulp.src('img/sprite/*.png').pipe(spritesmith({
         imgName: 'sprite.png',
+        // padding: 5,
         cssName: 'sprite.css'
     }));
 
@@ -32,15 +34,27 @@ gulp.task('sprite', function () {
 
     // Pipe CSS stream through CSS optimizer and onto disk
     var cssStream = spriteData.css
-        // .pipe(csso()) CSS를 압축한다.
+        //.pipe(csso()) //CSS를 압축한다.
         .pipe(gulp.dest('css/'));
 
     // Return a merged stream to handle both `end` events
     return merge(imgStream, cssStream);
 });
 
-gulp.task('watch', function () {
-    gulp.watch('css/**/*.scss', ['sass']);
+// gulp 서버 : https://www.npmjs.com/package/gulp-server-livereload
+gulp.task('webserver', function(){
+    gulp.src('./')
+        .pipe(server({
+            livereload: true,
+            open: true,
+            directoryListing: true
+            //defaultFile: 'index.html',
+            // port: 8888
+        }));
 });
 
-gulp.task('default', ['watch']);
+
+gulp.task('default', ['webserver']);
+
+
+// https://bl.ocks.org/twolfson/860a1d47e483bc34e1fa (Retina)
