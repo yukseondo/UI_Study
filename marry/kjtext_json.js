5b0726ae5933414a5af384f06b9ab48266be9077
DESKTOP-0H95RQO\user <yukseondo@naver.com>
2017-07-30 19:55:55 +0900

    marry add

---
+++ marry/kjtext_json.js
@@ -0,0 +1,53 @@
+{
+    "commonImg" : "visual.jpg",
+
+    "langko" : {
+        "title" : "<육선도·하마무라쇼코의 결혼식에 초대합니다>",
+        "subwayLine2" : "2호선 을지로 입구역 5번출구",
+        "subwayLine4" : "4호선 명동역 8번출구",
+        "address" : "서울 중구 명동길 61 로얄호텔",
+        "tel" : "02-2129-5830~2, 02-756-1112",
+        "greeting" : "같은 생각, 같은 마음으로 지혜롭게 살겠습니다.<br>저희 두 사람이 소중한 분들을 모시고<br>사랑의 결실을 이루려 합니다.<br>오로지 믿음과 사랑만을 약속하는 귀한 날에<br>축복의 걸음을 하시어 저희의 하나 됨을<br>지켜보아 주시고 격려해 주시면 <br>더없는 기쁨으로 간직하겠습니다.",
+        "date" : "2017년 10월 15일 일요일 낮 12시",
+        "eventroom" : "서울로얄호텔 3층 그랜드볼룸",
+        "telnumber" : "01051396262",
+        "telnumber2" : "01035706724",
+        "telnumber_father" : "01065372575",
+        "telnumber_mother" : "01051352575",
+        "telnumber_father_FIL" : "8109091348892",
+        "telnumber_mother_MIL" : "8108040035943",
+        "father" : "아버지 <strong>육종태</strong>",
+        "mother" : "어머니 <strong>이용자</strong>",
+        "father_FIL" : "아버지 <strong>하마무라히로미츠</strong>",
+        "mother_MIL" : "어머니 <strong>하마무라요코</strong>",
+        "parents_title1" : "신랑측 혼주",
+        "parents_title2" : "신부측 혼주",
+        "seondo" : "육선도",
+        "shoko" : "하마무라쇼코"
+    },
+
+    "langjp" : {
+        "title" : "<陸善道·濱村祥子の結婚式に招待します>",
+        "subwayLine2" : "2号線 乙支路入口駅(ウルチロ入口駅)5番出口",
+        "subwayLine4" : "4号線明洞駅(ミョンドン駅)8番出口",
+        "address" : "ソウル中区明洞道61ロイヤルホテル",
+        "tel" : "02-2129-5830~2, 02-756-1112",
+        "greeting" : "この度、結婚式を挙げることになりました。<br>同じ考え、同じ気持ちで、賢明に生きていきます。<br>私達二人が大切な方たちを仕えて、愛の結実を遂げます。愛を約束する貴重な日に、私たちが一つになったことを、見守り励ましていただきたく ご案内申し上げます。",
+        "date" : "2017年10月15日日曜日 昼12時",
+        "eventroom" : "ソウル ロイヤルホテル3階 グランドボールルーム",
+        "telnumber" : "01051396262",
+        "telnumber2" : "01035706724",
+        "telnumber_father" : "01065372575",
+        "telnumber_mother" : "01051352575",
+        "telnumber_father_FIL" : "8109091348892",
+        "telnumber_mother_MIL" : "8108040035943",
+        "father" : "父 <strong>ユク ジョンテ</strong>",
+        "mother" : "母 <strong> イ ヨンジャ</strong>",
+        "father_FIL" : "父 <strong>濱村 宏光</strong>",
+        "mother_MIL" : "母 <strong>濱村 陽子</strong>",
+        "parents_title1" : "新郎両親",
+        "parents_title2" : "新婦両親",
+        "seondo" : "陸善道",
+        "shoko" : "濱村祥子"
+    }
+}
